19c43ee353c2b09e435e56e63338a5661a8e62c0
blsonalib <blsonalib@gmail.com>
2019-12-17 14:59:24 +0530

    Added: code and test to compare two length objects of Feet type

--- src/main/java/com/quantitymeasurement/Length.java
+++ src/main/java/com/quantitymeasurement/Length.java
@@ -1,15 +1,27 @@
 package com.quantitymeasurement;
 
-import java.util.Objects;
-
 public class Length {
-    enum Unit{FEET,INCH};
+
+
+    private static final double FEET_TO_INCH = 12.0;
+
+    enum Unit {FEET, INCH;}
+
+    ;
     private final Unit unit;
     public double value;
 
-    public Length(Unit unit,double value) {
+    public Length(Unit unit, double value) {
         this.value = value;
         this.unit = unit;
+    }
+
+    public boolean compare(Length that) {
+        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.FEET))
+            return (Double.compare(this.value , that.value) == 0);
+        if (this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
+            return (Double.compare(this.value * FEET_TO_INCH, that.value) == 0);
+        return false;
     }
 
     @Override
@@ -20,5 +32,6 @@ public class Length {
         return Double.compare(length.value, value) == 0 &&
                 unit == length.unit;
     }
+
 
 }
--- src/test/java/com/quantitymeasurement/QuantityMasurementTest.java
+++ src/test/java/com/quantitymeasurement/QuantityMasurementTest.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 
 public class QuantityMasurementTest {
     @Test
-    public void givenOneFeetAndOneFeet_ShouldReturnTrue() {
-        Length feet1 = new Length(Length.Unit.FEET,1.0);
-        Length feet2 = new Length(Length.Unit.FEET,1.0);
+    public void given0FeetAnd0Feet_ShouldReturnTrue() {
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length feet2 = new Length(Length.Unit.FEET,0.0);
         Assert.assertEquals(feet1,feet2);
     }
 
     @Test
-    public void givenOneFeetAndZeroFeet_ShouldReturnFalse() {
+    public void given1FeetAnd0Feet_ShouldReturnFalse() {
         Length feet1 = new Length(Length.Unit.FEET,1.0);
         Length feet2 = new Length(Length.Unit.FEET,0.0);
         boolean equals = feet1.equals(feet2);
@@ -20,14 +20,14 @@ public class QuantityMasurementTest {
     }
 
     @Test
-    public void givenOneInchAndOneInch_ShouldReturnTrue() {
-        Length inch1 = new Length(Length.Unit.INCH,1.0);
-        Length inch2 = new Length(Length.Unit.INCH,1.0);
+    public void given0InchAnd0Inch_ShouldReturnTrue() {
+        Length inch1 = new Length(Length.Unit.INCH,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,0.0);
         Assert.assertEquals(inch1,inch2);
     }
 
     @Test
-    public void givenOneInchAndZeroInch_ShouldReturnTrue() {
+    public void given0InchAnd1Inch_ShouldReturnTrue() {
         Length inch1 = new Length(Length.Unit.INCH,1.0);
         Length inch2 = new Length(Length.Unit.INCH,0.0);
         boolean equals = inch1.equals(inch2);
@@ -35,10 +35,37 @@ public class QuantityMasurementTest {
     }
 
     @Test
-    public void given1FeetAnd1Inch_ShouldReturnNotEqual() {
-        Length feet1 = new Length(Length.Unit.FEET,1.0);
+    public void given0FeetAnd0Inch_ShouldReturnNotEqual() {
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
         Length inch2 = new Length(Length.Unit.INCH,0.0);
         boolean equals = feet1.equals(inch2);
         Assert.assertFalse(equals);
     }
+
+    @Test
+    public void givenZeroFeetAndZeroInch_ShouldReturnEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET,0.0);
+        Length inch2 = new Length(Length.Unit.INCH,0.0);
+        boolean compareCheck = feet1.compare(inch2);
+        Assert.assertTrue(compareCheck);
+    }
+
+    @Test
+    public void givenOneFeetAndOneInch_ShouldReturnNotEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET,1.0);
+        Length inch2 = new Length(Length.Unit.INCH,1.0);
+        boolean compareCheck = feet1.compare(inch2);
+        Assert.assertFalse(compareCheck);
+    }
+
+    @Test
+    public void givenOneFeetAndOneFeet_ShouldReturnNotEqualLength() {
+        Length feet1 = new Length(Length.Unit.FEET,1.0);
+        Length feet2 = new Length(Length.Unit.FEET,1.0);
+        boolean compareCheck = feet1.compare(feet2);
+        Assert.assertTrue(compareCheck);
+    }
+
+
+
 }
